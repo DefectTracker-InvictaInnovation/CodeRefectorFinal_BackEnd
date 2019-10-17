ae5511601cd02afc65cdd694623c894306cd3357
romipraveen1 <romipraveen100@gmail.com>
2019-10-17 10:49:42 +0530

    Changes

--- sgic-internal-services/login-service/src/main/java/com/sgic/internal/login/controller/AuthRestAPIs.java
+++ sgic-internal-services/login-service/src/main/java/com/sgic/internal/login/controller/AuthRestAPIs.java
@@ -170,7 +170,7 @@ public class AuthRestAPIs {
 		}
 
 		// Creating user's account
-		User user = new User(signUpRequest.getName(), signUpRequest.getUsername(), signUpRequest.getEmail(),
+		User user = new User(signUpRequest.getName(),signUpRequest.getLastname(), signUpRequest.getUsername(), signUpRequest.getEmail(),
 				encoder.encode(signUpRequest.getPassword()));
 
 	String strRoles = signUpRequest.getRole();
